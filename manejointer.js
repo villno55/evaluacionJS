e01b7acaf0eee789d2067879684874aef4366865
villno55 <adsotrabajos24@gmail.com>
2025-09-09 09:32:27 -0500

    Update manejointer.js

--- manejointer.js
+++ manejointer.js
@@ -1,7 +1,16 @@
 export function actualizarUI(datos) {
-  document.getElementById("txtAutos").value = datos.autos;
+  document.getElementById("txtCarros").value = datos.carros;
   document.getElementById("txtBuses").value = datos.buses;
   document.getElementById("txtCamiones").value = datos.camiones;
   document.getElementById("txtTotal").value = "$ " + datos.total;
 }
+
+export function habilitarControles(estado) {
+  document.getElementById("btnCarro").disabled = !estado;
+  document.getElementById("btnBus").disabled = !estado;
+  document.getElementById("btnCamion").disabled = !estado;
+  document.getElementById("btnCerrar").disabled = !estado;
+  document.getElementById("btnAbrir").disabled = estado;
+}
+
 
