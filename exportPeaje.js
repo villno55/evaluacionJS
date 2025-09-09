5f71e65da7b774ebf53d8c2ababbbefee3d204ef
villno55 <adsotrabajos24@gmail.com>
2025-09-09 09:30:55 -0500

    hecho

--- exportPeaje.js
+++ exportPeaje.js
@@ -1,11 +1,38 @@
-export default class Peaje {
+export default class pasaPeaje {
   constructor() {
-    this.autos = 0;
+    this.carros = 0;
     this.buses = 0;
     this.camiones = 0;
     this.total = 0;
-    this.precios = { auto: 5000, bus: 10000, camion: 15000 };
+    this.costo = { carro: 5000, bus: 10000, camion: 15000 };
   }
+
+  registrar(tipo) {
+    if (tipo === "carro") { this.carros++; this.total += this.costo.carro; }
+    if (tipo === "bus") { this.buses++; this.total += this.costo.bus; }
+    if (tipo === "camion") { this.camiones++; this.total += this.costo.camion; }
+  }
+
+  datos() {
+    return {
+      carros: this.carros,
+      buses: this.buses,
+      camiones: this.camiones,
+      total: this.total
+    };
+  }
+
+  reiniciar() {
+    this.carros = 0;
+    this.buses = 0;
+    this.camiones = 0;
+    this.total = 0;
+  }
+}
+
+
+
+
 
   
 
