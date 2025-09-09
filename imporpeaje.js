b53de9743d9c7fb9ded16f7d98467b9e5f277f0b
villno55 <adsotrabajos24@gmail.com>
2025-09-09 09:31:42 -0500

    listo

--- imporpeaje.js
+++ imporpeaje.js
@@ -1,7 +1,42 @@
-import Peaje from "./peaje.js";
-import { actualizarUI, habilitarControles } from "./mostrando.js";
+import Peaje from "./pasaPeaje.js";
+import { actualizarUI, habilitarControles } from "./actualizaUser.js";
 
 const peaje = new Peaje();
+
+
+const btnAbrir = document.getElementById("btnAbrir");
+const btnCerrar = document.getElementById("btnCerrar");
+const btnCarro = document.getElementById("btnCarro");
+const btnBus = document.getElementById("btnBus");
+const btnCamion = document.getElementById("btnCamion");
+
+
+btnAbrir.addEventListener("click", () => {
+  peaje.reiniciar();
+  actualizarUI(peaje.datos());
+  habilitarControles(true);
+});
+
+btnCerrar.addEventListener("click", () => {
+  habilitarControles(false);
+});
+
+btnCarro.addEventListener("click", () => {
+  peaje.registrar("carro");
+  actualizarUI(peaje.datos());
+});
+
+btnBus.addEventListener("click", () => {
+  peaje.registrar("bus");
+  actualizarUI(peaje.datos());
+});
+
+btnCamion.addEventListener("click", () => {
+  peaje.registrar("camion");
+  actualizarUI(peaje.datos());
+});
+
+
 
 
 
